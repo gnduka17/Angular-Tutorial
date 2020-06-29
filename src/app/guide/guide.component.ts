6f03de2b4df117584c13a1b356381003e3cd3f9c
gnduka17 <nduka@usc.edu>
2020-06-29 13:11:17 -0700

    copy elements

--- src/app/guide/guide.component.ts
+++ src/app/guide/guide.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { GuideStatesService } from "../guide-states.service";
+import { Clipboard } from "@angular/cdk/clipboard"
+import {ClipboardModule} from '@angular/cdk/clipboard';
 
 @Component({
   selector: "app-guide",
@@ -8,66 +10,75 @@ import { GuideStatesService } from "../guide-states.service";
 })
 export class GuideComponent implements OnInit {
   currentStep = 0;
+
+  copyInputMessage(inputElement){
+    console.log(inputElement);
+    var elm = document.getElementById(inputElement);
+  // for Internet Explorer
+
+  // if(document.body.createTextRange) {
+  //   var range = document.body.createTextRange();
+  //   range.moveToElementText(elm);
+  //   range.select();
+  //   document.execCommand("Copy");
+  //   alert("Copied div content to clipboard");
+  // }
+  if(window.getSelection) {
+    // other browsers
+
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(elm);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand("Copy");
+    alert("Copied div content to clipboard");
+  }
+    // inputElement.select();
+    // document.execCommand('copy');
+    // inputElement.setSelectionRange(0, 0);
+  }
   content = [
     // ~~~~~~ PART 1 INTRO ~~~~~~
-    "<h2><strong>Part 1: Getting Started With a Basic Angular App</strong></h2><hr> \
-  <ul> \
-  <li> <p> You are now staring at an online IDE called Stackblitz. You can  create Angular and React projects that are immediately online & shareable via link in just one click.</p></li> \
-  <li> <p>This pane, on the right, shows the starting state of the sample Angular app. It defines a frame with a top bar ...containing the store name and checkout icon...and the title for a product list which will be populated and dynamically updated with data from the application).</p> </li> \
-  <li> <p>The project pane on the left is where you will be coding. It shows the source files that make up the application, including all of the infrastructure and configuration files. The currently selected file shows up in the editor pane in the middle.</p> </li> \
-	<li> <p>Before going into the source structure, the next section shows how to fill out the HTML <em>template</em> for the product list, using the provided sample data. This should give you an idea how easy it is to modify and update the page dynamically.</p> </li> \
-  </ul>",
+  "<h1 class=\"titleH\">Part 1: Getting Started With a Basic Angular App</h1><hr> \
+  <div #userinput>\
+    <h2>Products</h2>\
+    <div *ngFor=\"let product of products\">\
+    </div>\
+  </div>\
+  <button (click)=\"copyInputMessage(userinput)\" value=\"click to copy\" >Copy from Textbox</button>\
+  <p class = \"guideText\"> You are now staring at an online IDE called Stackblitz. You can  create Angular and React projects that are immediately online & shareable via link in just one click.</p>",
+  "<p class = \"guideText\">This pane, on the right, shows the starting state of the sample Angular app. It defines a frame with a top bar (containing the store name and checkout icon) and the title for a product list which will be populated and dynamically updated with data from the application).</p>",
+  "<p class = \"guideText\">The project pane on the left is where you will be coding. It shows the source files that make up the application, including all of the infrastructure and configuration files. The currently selected file shows up in the editor pane in the middle.</p>",
+	// "<p class = \"guideText\">The next section will show you how to fill out the HTML <em>template</em> for the product list, using the provided sample data. This should give you an idea how easy it is to modify and update the page dynamically.</p>",
 
     // STACKBLITZ TIPS
-    "<div> \
-	<h2><strong>StackBlitz tips</strong></h2> <hr> \
-	<ul> \
-		<li> \
-    <p>Log into StackBlitz so you can save and resume your work. \
+	"<h2>StackBlitz tips</h2> <hr> \
+    <p class = \"guideText\">Log into StackBlitz so you can save and resume your work. \
 				If you have a GitHub account, you can log into StackBlitz \
-				with that account. In order to save your progress, first \
+				with that account.<br><br> In order to save your progress, first \
 				fork the project using the Fork button at the top left, \
 				then you'll be able to save your work to your own StackBlitz \
-				account by clicking the Save button.</p> \
-		</li> \
-		<li> \
-			<p>To copy a code example from this tutorial, click the icon \
-				at the top right of the code example box, and then paste the \
-				code snippet from the clipboard into StackBlitz.</p> \
-		</li> \
-		<li> \
-			<p>If the StackBlitz preview pane isn't showing what you \
+				account by clicking the Save button.</p>",
+			// "<h1>StackBlitz tips</h1> <hr> <p class = \"guideText\">To copy a code example from this tutorial, click the icon \
+			// 	at the top right of the code example box, and then paste the \
+			// 	code snippet from the clipboard into StackBlitz.</p>",
+			"<h2>StackBlitz tips</h2> <hr> <p class = \"guideText\">If the StackBlitz preview pane isn't showing what you \
 				expect, save and then click the refresh button.</p> \
-		</li> \
-		<li> \
-			<p>StackBlitz is continually improving, so there may be \
+			<p class = \"guideText\">StackBlitz is continually improving, so there may be \
 				slight differences in generated code, but the app's \
-				behavior will be the same.</p> \
-		</li> \
-		<li> \
-			<p>When you generate the StackBlitz example apps that \
+				behavior will be the same.</p>",
+			"<h2>StackBlitz tips</h2> <hr> <p class = \"guideText\">When you generate the StackBlitz example apps that \
 				accompany the tutorials, StackBlitz creates the starter \
 				files and mock data for you. The files you'll use throughout \
 				the tutorials are in the <code>src</code> folder of the StackBlitz \
-        example apps.</p> \
-		</li> \
-	</ul> \
-</div>",
+        example apps.</p>",
 
-    // TEMPLATE SYNTAX
-    '<h2><strong>Template syntax</strong></h2> <hr> \
-<p>Angular\'s template syntax extends HTML and JavaScript. \
-  This section introduces template syntax by enhancing the "Products" area. \
-</p> \
-<p>To help you get going, the following steps use predefined product data from the \
-	<em><strong>products.ts</strong></em> file \
-	(already created in StackBlitz example) and methods from the <em><strong>product-list.component.ts</strong></em> \
-	file. \
-</p>',
-    // STEP 1
-    "<p>In the <em><strong>product-list</strong></em> folder, open the template file <em><strong>product-list.component.html</strong></em>.</p>",
+    // TEMPLATE SYNTAX...STEP 1
+    '<h2><strong>Now Let\'s Get To It!</strong></h2> <hr> \
+    <p class = \"guideText\"> <em><strong>STEP 1:</strong></em> In the <em><strong>product-list</strong></em> folder, open the template file <em><strong>product-list.component.html</strong></em>.</p>',
     // STEP 2
-    '<p>Modify the product list template to display a list of product names.</p> \
+    '<p class = \"guideText\"> <em><strong>STEP 2:</strong></em> Modify the product list template to display a list of product names.</p> \
 <ol> \
 	<li> \
 		<p>Each product in the list displays the same way, one after another on the page. To iterate over the predefined \
@@ -88,41 +99,29 @@ export class GuideComponent implements OnInit {
 	</li>\
 </ol>',
 // STEP 3
-"<li>\
-	<p>To make each product name a link to product details, add the <code>&lt;a&gt;</code> element and set its title to\
+" <p class = \"guideText\"><em><strong>STEP 3:</strong></em> To make each product name a link to product details, add the <code>&lt;a&gt;</code> element and set its title to\
 		be the product's name by using the property binding <code>[ ]</code> syntax, as follows:</p>\
 	<p>In the preview pane, hold the pointer over a product\
 		name to see the bound name property value, which is\
 		the product name plus the word \"details\".\
 		Interpolation <code>{{ }}</code> lets you render the\
 		property value as text; property binding <code>[ ]</code> lets you\
-		use the property value in a template expression.</p>\
-</li>",
+		use the property value in a template expression.</p>",
 // STEP 4
-"<li>\
-	<p>Add the product descriptions. On the <code>&lt;p&gt;</code> element, use an\
+"<p class = \"guideText\"> <em><strong>STEP 4:</strong></em> Add the product descriptions. On the <code>&lt;p&gt;</code> element, use an\
 		<code>*<a href=\"api/common/NgIf\" class=\"code-anchor\">ngIf</a></code> directive so that Angular only creates the\
 		<code>&lt;p&gt;</code> element if the current product has a description.</p>\
 	<p>The app now displays the name and description of each product in the list. Notice that the final product does not\
 		have a description paragraph. Because the product's description property is empty, Angular doesn't create the\
-		<code>&lt;p&gt;</code> element—including the word \"Description\".</p>\
-</li>",
+		<code>&lt;p&gt;</code> element—including the word \"Description\".</p>",
 // STEP 5
-"<li>\
-	<p>Add a button so users can share a product with friends. Bind the button's <code>click</code> event to the\
+"<p class = \"guideText\"> <em><strong>STEP 5:</strong></em>. Add a button so users can share a product with friends. Bind the button's <code>click</code> event to the\
 		<code>share()</code> method (in <code>product-list.component.ts</code>). Event binding uses a set of\
 		parentheses, <code>( )</code>, around the event, as in the following <code>&lt;button&gt;</code> element:</p>\
 	<p>Each product now has a \"Share\" button:</p>\
-	<div class=\"lightbox\">\
-		<img src=\"generated/images/guide/start/template-syntax-product-share-button.png\" alt=\"Share button added for each product\" width=\"259\" height=\"376\">\
-  </div>\
-	<p>Test the \"Share\" button:</p>\
-	<div class=\"lightbox\">\
-			<img src=\"generated/images/guide/start/template-syntax-product-share-alert.png\" alt=\"Alert box indicating product has been shared\" width=\"330\" height=\"104\">\
-  </div>\
-</li>",
+	<p>Test the \"Share\" button:</p>",
 // CONCLUDE TEMPLATE SYNTAX
-"<p>The app now has a product list and sharing feature.\
+"<p class = \"guideText\">The app now has a product list and sharing feature.\
 	In the process, you've learned to use five common features of Angular's template syntax:</p>\
 <ul>\
 	<li>*ngFor</li>\
@@ -130,12 +129,7 @@ export class GuideComponent implements OnInit {
 	<li>Interpolation <code>{{ }}</code></li>\
 	<li>Property binding <code>[ ]</code></li>\
 	<li>Event binding <code>( )</code></li>\
-</ul>\
-<div>\
-	<p>For a fuller introduction to Angular's template syntax, see <a\
-			href=\"guide/architecture-components#template-syntax\" title=\"Template Syntax\">Introduction to components and\
-			templates</a>.</p>\
-</div>",
+</ul>",
 // COMPONENTS INTRO
 "<h2>Components</h2>\
 <p><em>Components</em> define areas of responsibility in the user interface, or UI,\
